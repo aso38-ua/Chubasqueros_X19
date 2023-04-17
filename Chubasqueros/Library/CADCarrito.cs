1b86a8e7807f1edd78a4d4f9086f11e6842bb59c
mje5-ua <mje5@alu.ua.es>
2023-04-17 12:58:38 +0200

    Pull1

---
+++ Chubasqueros/Library/CADCarrito.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+using System.Configuration;
+
+
+namespace Library
+{
+    class CADCarrito
+    {
+        private String conection; //Conexion con la BB.DD
+        public CADCarrito()
+        {
+            
+        }
+
+
+        //Cuenta la cantidad que ha escogido el usuario sobre 1 producto
+        public int cuentaCantidad()
+        {
+
+        }
+
+        //Añadir producto al carrito
+        public bool AñadirProducto()
+        {
+
+        }
+
+        //Eliminar producto del carrito
+        public bool EliminarProducto()
+        {
+
+        }
+
+        //Sacar el producto de la cesta y guardarlo en un subcarrito aparte
+        public bool GuardarProducto()
+        {
+
+        }
+
+
+        //Cuenta todos los productos que ha pedido el usuario (Diferentes productos)
+        public int ProductosTotales()
+        {
+
+        }
+
+        //Calcula el precio total que hay en el carrito
+        public int PrecioTotal()
+        {
+
+        }
+
+
+        //Boton de comprar exclusivo para carrito
+        public bool Comprar()
+        {
+
+        }
+    }
+}
---
+++ Chubasqueros/Library/ENCarrito.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class ENCarrito
+    {
+        private int cantidad;
+        private int total; //precio total
+        private int numprod; //numero de productos totales que hay
+
+
+        public int c
+        {
+            get { return cantidad; }
+            set { cantidad = value; }
+        }
+
+        public int t
+        {
+            get { return total; }
+            set { total = value; }
+        }
+
+        public int n
+        {
+            get { return numprod; }
+            set { numprod = value; }
+        }
+
+        /*Constructor: pondra la cantidad a 0, el total a 0, todos los productos (numprod) = 0,
+         la clase se enlazara con usuario y producto*/
+        public ENCarrito()
+        {
+            c = 0;
+            t = 0;
+            numprod = 0;
+
+        }
+
+        public ENCarrito(int cant, int tot, int np)
+        {
+            c = cant;
+            t = tot;
+            n = np;
+        }
+
+        //Cuenta la cantidad que ha escogido el usuario sobre 1 producto
+        public int cuentaCantidad()
+        {
+
+        }
+
+        //Añadir producto al carrito
+        public AñadirProducto()
+        {
+
+        }   
+
+        //Eliminar producto del carrito
+        public EliminarProducto()
+        {
+
+        }
+
+        //Sacar el producto de la cesta y guardarlo en un subcarrito aparte
+        public GuardarProducto()
+        {
+
+        }
+
+
+        //Cuenta todos los productos que ha pedido el usuario (Diferentes productos)
+        public int ProductosTotales()
+        {
+
+        }
+
+        //Calcula el precio total que hay en el carrito
+        public int PrecioTotal()
+        {
+
+        }
+
+
+        //Boton de comprar exclusivo para carrito
+        public Comprar()
+        {
+
+        }
+   
+
+
+
+    }
+}
