659b033e97b40c8d8eb8f31b0889219895cd31c8
fmpp3-ua <78901771+fmpp3-ua@users.noreply.github.com>
2023-04-19 10:03:27 +0200

    Usuario y Colaboracion hecha

---
+++ Chubasqueros/Library/CADUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+using System.Configuration;
+
+namespace library
+{
+    public class CADUsuario
+    {
+        private String constring;
+
+        public CADUsuario()
+        {
+            constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+        public bool createUsuario(ENUsuario en) { return true; }
+        public bool readUsuario(ENUsuario en) { return true; }
+        public bool updateUsuario(ENUsuario en) { return true; }
+        public bool deleteUsuario(ENUsuario en) { return true; }
+    }
+}
---
+++ Chubasqueros/Library/ENUsuario.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+using System.Configuration;
+
+namespace Library
+{
+    class ENUsuario
+    {
+        //Atributos privados
+        private int Id;
+        private string Nombre;
+        private string Apellidos;
+        private string NIF;
+        private string Email;
+        private string Contraseña;
+
+        //Getters y setters
+        public string intId
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+
+        public string stringNombre
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+
+        public string stringApellidos
+        {
+            get { return Apellidos; }
+            set { Apellidos = value; }
+        }
+
+        public string stringEmail
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+
+        public string stringContraseña
+        {
+            get { return Contraseña; }
+            set { Contraseña = value; }
+        }
+
+        public string stringNif
+        {
+            get { return NIF; }
+            set { NIF = value; }
+        }
+
+        //Constructor por defecto
+        public ENUsuario() {
+            Id = 0;
+            Nombre = string.Empty;
+            Apellidos = string.Empty;
+            Email = string.Empty;
+            NIF = string.Empty;
+            Contraseña = string.Empty;
+        }
+
+        //Constructor copia
+        public ENUsuario(int id, string email, string nombre,string apellidos,string nif, string contraseña) {
+            Id = id;
+            Nombre = nombre;
+            Apellidos = apellidos;
+            Email = email;
+            NIF = nif;
+            Contraseña = contraseña;
+        }
+
+        //Crear un usuario
+        public bool createUsuario() {
+            CADUsuario user = new CADUsuario();
+            bool create = false;
+            if (!user.readUsuario(this))
+                creaate = user.createUsuario(this);
+            return created; 
+        }
+
+        //Leer un usuario de la  base de datos
+        public bool readUsuario() {
+            CADUsuario user = new CADUsuario();
+            bool read = user.readUsuario(this);
+            return read; 
+        }
+
+        //Modificar un usuario
+        public bool updateUsuario() {
+            ENUsuario aux = new ENUsuario(this);
+            CADUsuario user = new CADUsuario();
+            bool update = false;
+            if (user.readReserva(aux))
+            {
+                this.Id = aux.Id;
+                this.Nombre = aux.Nombre;
+                this.Apellidos = aux.Apellidos;
+                this.NIF = aux.NIF;
+                this.Email = aux.Email;
+                this.Contraseña=aux.Contraseña
+                updated = user.updateUsuario(this);
+            }
+            return update;
+        }
+        
+        //Elimina un usuario
+        public bool deleteUsuario() {
+            CADUsuario user = new CADUsuario();
+            bool eliminado = false;
+            if (user.readUsuario(this))
+                eliminado = user.deleteUsuario(this);
+            else{return eliminado;} 
+        }
+    }
+}
