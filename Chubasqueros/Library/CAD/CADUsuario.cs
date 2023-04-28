dc9171346356b18186ff590d3a2ba80fa7906f84
aso38 <aso38@alu.ua.es>
2023-04-28 02:27:22 +0200

    A

---
+++ Chubasqueros/Library/CAD/CADUsuario.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class CADUsuario
+    {
+        private String constring;
+        public CADUsuario()
+        {
+            constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+        public bool createUsuario(ENUsuario en) { return true; }
+        public bool readUsuario(ENUsuario en) { return true; }
+        public bool updateUsuario(ENUsuario en) { return true; }
+        public bool deleteUsuario(ENUsuario en) { return true; }
+        // Create
+        public static void CrearUsuario(ENUsuario en)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Read
+        public static ENUsuario ObtenerUsuarioPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ENUsuario ObtenerUsuarioPorEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<ENUsuario> ObtenerTodosLosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Update
+        public static void ActualizarUsuario(ENUsuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Delete
+        public static void EliminarUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chubasqueros/Library/EN/ENUsuario.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class ENUsuario
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public string contraseña { get; set; }
+
+        public ENUsuario()
+        {
+            this.id = 0;
+            this.nombre = "";
+            this.apellido = "";
+            this.email = "";
+            this.contraseña="";
+        }
+
+        public ENUsuario(int id, string nombre, string apellido, string email, string contraseña)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.email = email;
+            this.contraseña = contraseña;
+        }
+
+        //Crear usuario
+        public bool createUsuario()
+        {
+            CADUsuario user = new CADUsuario();
+            bool create = false;
+            if (!user.readUsuario(this))
+                create = user.createUsuario(this);
+            return create;
+        }
+
+        //Lee un usuario de la base de datos
+        public bool readUsuario()
+        {
+            CADUsuario user = new CADUsuario();
+            bool read = user.readUsuario(this);
+            return read;
+        }
+
+        //Modifica un usuario
+        public bool updateUsuario()
+        {
+            ENUsuario aux = new ENUsuario();
+            CADUsuario user = new CADUsuario();
+            bool update = false;
+            if (user.readUsuario(aux))
+            {
+                this.id = id;
+                this.nombre = nombre;
+                this.apellido = apellido;
+                this.email = email;
+                this.contraseña = contraseña;
+                update = user.updateUsuario(this);
+            }
+            return update;
+        }
+
+        //Elimina un usuario
+        public bool deleteUsuario()
+        {
+            CADUsuario user = new CADUsuario();
+            bool eliminado = false;
+            if (user.readUsuario(this))
+                eliminado = user.deleteUsuario(this);
+            return eliminado;
+        }
+
+        // Create
+        public static void CrearUsuario(int id, string nombre, string apellido, string email, string contraseña)
+        {
+            ENUsuario nuevoUsuario = new ENUsuario(id, nombre, apellido, email, contraseña);
+            CADUsuario.CrearUsuario(nuevoUsuario);
+        }
+
+        // Read
+        public static ENUsuario ObtenerUsuarioPorId(int id)
+        {
+            return CADUsuario.ObtenerUsuarioPorId(id);
+        }
+
+        public static ENUsuario ObtenerUsuarioPorEmail(string email)
+        {
+            return CADUsuario.ObtenerUsuarioPorEmail(email);
+        }
+
+        public static List<ENUsuario> ObtenerTodosLosUsuarios()
+        {
+            return CADUsuario.ObtenerTodosLosUsuarios();
+        }
+
+        // Update
+        public void ActualizarNombre(string nuevoNombre)
+        {
+            this.nombre = nuevoNombre;
+            CADUsuario.ActualizarUsuario(this);
+        }
+
+        public void ActualizarApellido(string nuevoApellido)
+        {
+            this.apellido = nuevoApellido;
+            CADUsuario.ActualizarUsuario(this);
+        }
+
+        public void ActualizarEmail(string nuevoEmail)
+        {
+            this.email = nuevoEmail;
+            CADUsuario.ActualizarUsuario(this);
+        }
+
+        public void ActualizarContraseña(string nuevaContraseña)
+        {
+            this.contraseña = nuevaContraseña;
+            CADUsuario.ActualizarUsuario(this);
+        }
+
+        // Delete
+        public void Eliminar()
+        {
+            CADUsuario.EliminarUsuario(this.id);
+        }
+    }
+}
