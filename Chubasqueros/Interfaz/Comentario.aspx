439a7634cfabe1dd149f604d9d688b9746a2377f
jsf34-ua <78562601+jsf34-ua@users.noreply.github.com>
2023-04-29 20:51:39 +0200

    Esqueleto interfaz comentario/puntuación

---
+++ Chubasqueros/Interfaz/Comentario.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Comentario.aspx.cs" Inherits="Interfaz.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="body" runat="server">
+    <h2> Comentarios</h2>
+
+    <asp:Button ID="BtnPuntuar" runat="server" Text="Puntuar" onClick="PuntuarClick" BackColor= "#00ffff"/>
+    <asp:Button ID="Estrella1" runat="server" Text="Estrella1" onClick="Estrella1Click" BackColor="Yellow"/>
+    <asp:Button ID="Estrella2" runat="server" Text="Estrella2" onClick="Estrella2Click" BackColor="Yellow"/>
+    <asp:Button ID="Estrella3" runat="server" Text="Estrella3" onClick="Estrella3Click" BackColor="Yellow"/>
+    <asp:Button ID="Estrella4" runat="server" Text="Estrella4" onClick="Estrella4Click" BackColor="Yellow"/>
+    <asp:Button ID="Estrella5" runat="server" Text="Estrella5" onClick="Estrella5Click" BackColor="Yellow"/>
+    <br />
+    <asp:Button ID="BtnComentar" runat="server" Text="Comentar" onClick="ComentarClick"/>
+    <br />
+    Comentario:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TBComentario" runat="server"></asp:TextBox>
+    <br />
+    <asp:Button ID="Like" runat="server" Text="Like" onClick="LikeClick" BackColor= "#00ff00"/>
+    <asp:Button ID="Dislike" runat="server" Text="Dislike" onClick="DislikeClick" BackColor= "#ff0000"/>
+    <br />
+    <asp:Button ID="BtnRespuesta" runat="server" Text="Responder" onClick="RespuestaClick"/>
+    <br />
+    Responder:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+</asp:Content>
---
+++ Chubasqueros/Interfaz/Comentario.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Interfaz
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control BtnPuntuar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnPuntuar;
+
+        /// <summary>
+        /// Control Estrella1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Estrella1;
+
+        /// <summary>
+        /// Control Estrella2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Estrella2;
+
+        /// <summary>
+        /// Control Estrella3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Estrella3;
+
+        /// <summary>
+        /// Control Estrella4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Estrella4;
+
+        /// <summary>
+        /// Control Estrella5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Estrella5;
+
+        /// <summary>
+        /// Control BtnComentar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnComentar;
+
+        /// <summary>
+        /// Control TBComentario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBComentario;
+
+        /// <summary>
+        /// Control Like.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Like;
+
+        /// <summary>
+        /// Control Dislike.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Dislike;
+
+        /// <summary>
+        /// Control BtnRespuesta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnRespuesta;
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+    }
+}
