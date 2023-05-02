b8038878a6c5b6d08600e06dc5b85aad7c09831b
aso38 <aso38@alu.ua.es>
2023-05-02 13:51:12 +0200

    Mejoreado el registro, ahora los campos no se pueden dejar en blanco y el email debe tener unas caracteristicas especiales

--- Chubasqueros/Interfaz/Perfil.aspx.cs
+++ Chubasqueros/Interfaz/Perfil.aspx.cs
@@ -31,9 +31,9 @@ namespace Interfaz
                     }
                     reader.Close();
                 }
-            }
-        }*/
+            }*/
         }
+    
 
         /*protected void btnSave_Click(object sender, EventArgs e)
         {
