60dff9c3dd759515ee4be6dc8b90bb2c4e8058ab
jsf34 <jsf34@alu.ua.es>
2023-05-03 19:56:21 +0200

    Actualización para que se vea la intefaz correctamente

--- Chubasqueros/Interfaz/Comentario.aspx.cs
+++ Chubasqueros/Interfaz/Comentario.aspx.cs
@@ -13,5 +13,47 @@ namespace Interfaz
         {
 
         }
+
+        protected void Estrella1Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Estrella2Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void Estrella3Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void Estrella4Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void Estrella5Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void PuntuarClick(object sender, EventArgs e)
+        {
+
+        }
+        protected void ComentarClick(object sender, EventArgs e)
+        {
+
+        }
+        protected void LikeClick(object sender, EventArgs e)
+        {
+
+        }
+        protected void DislikeClick(object sender, EventArgs e)
+        {
+
+        }
+        protected void RespuestaClick(object sender, EventArgs e)
+        {
+
+        }
     }
 }
