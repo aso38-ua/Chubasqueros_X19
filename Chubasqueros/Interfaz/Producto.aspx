a5c151ebd258c633c9cd15d829a07593a70de0b7
Joan Catasús Quesada <jcqc2@mscloud.ua.es>
2023-05-05 04:30:17 +0200

    DNI: 77853871K, fallos solucionados

--- Chubasqueros/Interfaz/Producto.aspx
+++ Chubasqueros/Interfaz/Producto.aspx
@@ -11,18 +11,18 @@
             codigo: &nbsp;<asp:TextBox ID="text_codigo" runat="server" Height="15px" style="color:deeppink; margin-top: 10px; margin-left: 15px;" Width="200px"></asp:TextBox>
         </p>
         <p>
-            nombre: &nbsp;<asp:TextBox ID="Text_nombre" runat="server" Height="15px" style="color:deeppink; margin-top: 10px; margin-left: 15px;" Width="200px"></asp:TextBox>
+            nombre: &nbsp;<asp:TextBox ID="text_nombre" runat="server" Height="15px" style="color:deeppink; margin-top: 10px; margin-left: 15px;" Width="200px"></asp:TextBox>
         </p>
         <p>
-            descripcion: &nbsp;<asp:TextBox ID="Text_descripcion" runat="server" Height="15px" style="color:deeppink; margin-top: 10px; margin-left: 15px;" Width="200px"></asp:TextBox>
+            descripcion: &nbsp;<asp:TextBox ID="text_descripcion" runat="server" Height="15px" style="color:deeppink; margin-top: 10px; margin-left: 15px;" Width="200px"></asp:TextBox>
         </p>
         <p>
             stock: &nbsp;<asp:TextBox ID="text_stock" runat="server" Height="15px" style="color:deeppink; margin-top: 10px; margin-left: 15px;" Width="200px"></asp:TextBox>
         </p>
         <p>
-            precio: &nbsp;<asp:TextBox ID="Text_precio" runat="server" Height="15px" style="color:deeppink; margin-top: 10px; margin-left: 15px;" Width="200px"></asp:TextBox>
+            precio: &nbsp;<asp:TextBox ID="text_precio" runat="server" Height="15px" style="color:deeppink; margin-top: 10px; margin-left: 15px;" Width="200px"></asp:TextBox>
         </p>
-        <p>
+         <p>
             codigoCategoria: &nbsp;<asp:TextBox ID="text_codigoCategoria" runat="server" Height="15px" style="color:deeppink; margin-top: 10px; margin-left: 15px;" Width="200px"></asp:TextBox>
         </p>
     </div>
@@ -40,6 +40,5 @@
     <asp:Button text="Crear producto" onClick="onCrear" ID="buttom_Crear" runat="server" />
     <asp:Button text="Actualizar producto" onClick="onActualizar" ID="buttom_Actualizar" runat="server" Width="148px" />
     <asp:Button text="Borrar producto" onClick="onBorrar" ID="buttom_Borrar" runat="server" Width="130px" />
-
 
 </asp:Content>
--- Chubasqueros/Interfaz/Producto.aspx.cs
+++ Chubasqueros/Interfaz/Producto.aspx.cs
@@ -16,7 +16,7 @@ namespace Interfaz
 
         protected void onLeer(object sender, EventArgs e)
         {
-            if (text_codigo.Text == "")
+            /**if (text_codigo.Text == "")
                 outputMsg.Text = "Clave primaria de producto no introducida.";
             else
             {
@@ -34,12 +34,12 @@ namespace Interfaz
                     outputMsg.Text = "Producto " + producto.getNombre() + ", " + producto.getCodigo();
                 }
                 else outputMsg.Text = "Producto no encontrado en la B.D.";
-            }
+            }*/
         }
 
         protected void onCrear(object sender, EventArgs e)
         {
-            if (text_nombre.Text != "" && text_codigo.Text != "" && text_descripcion.Text != "" && text_stock.Text != "" && text_precio.Text != "" && text_codigoCategoria.Text != "")
+            /**if (text_nombre.Text != "" && text_codigo.Text != "" && text_descripcion.Text != "" && text_stock.Text != "" && text_precio.Text != "" && text_codigoCategoria.Text != "")
             {
                 ENProducto producto = new ENProducto(text_codigo.Text, text_nombre.Text, text_descripcion.Text, text_stock.Text, text_precio.Text, text_codigoCategoria.Text);
 
@@ -48,12 +48,12 @@ namespace Interfaz
                 else outputMsg.Text = "No es posible insertar el producto.";
             }
 
-            else outputMsg.Text = "Alguno de los campos no estan especificados.";
+            else outputMsg.Text = "Alguno de los campos no estan especificados.";*/
         }
 
         protected void onActualizar(object sender, EventArgs e)
         {
-            if (text_nombre.Text != "" && text_codigo.Text != "" && text_descripcion.Text != "" && text_stock.Text != "" && text_precio.Text != "" && text_codigoCategoria.Text != "")
+            /**if (text_nombre.Text != "" && text_codigo.Text != "" && text_descripcion.Text != "" && text_stock.Text != "" && text_precio.Text != "" && text_codigoCategoria.Text != "")
             {
                 ENProducto producto = new ENProducto(text_codigo.Text, text_nombre.Text, text_descripcion.Text, text_stock.Text, text_precio.Text, text_codigoCategoria.Text);
 
@@ -64,13 +64,13 @@ namespace Interfaz
                 else outputMsg.Text = "Este producto no existe en la B.D.";
             }
 
-            else outputMsg.Text = "Alguno de los campos no estan especificados.";
+            else outputMsg.Text = "Alguno de los campos no estan especificados.";*/
 
         }
 
         protected void onBorrar(object sender, EventArgs e)
         {
-            if (text_nombre.Text != "" && text_codigo.Text != "")
+            /**if (text_nombre.Text != "" && text_codigo.Text != "")
             {
                 ENProducto producto = new ENProducto(text_codigo.Text, text_nombre.Text, text_descripcion.Text, text_stock.Text, text_precio.Text, text_codigoCategoria.Text);
 
@@ -80,7 +80,7 @@ namespace Interfaz
 
             }
 
-            else outputMsg.Text = "Alguno de los campos no estan especificados.";
+            else outputMsg.Text = "Alguno de los campos no estan especificados.";*/
         }
 
         protected void onComprar(object sender, EventArgs e)
@@ -115,7 +115,7 @@ namespace Interfaz
 
         protected void onCategoria(object sender, EventArgs e)
         {
-            if (text_codigo.Text == "" || text_codigoCategoria.Text == "")
+            /**if (text_codigo.Text == "" || text_codigoCategoria.Text == "")
                 outputMsg.Text = "Clave primaria de producto no introducida.";
             else
             {
@@ -143,7 +143,7 @@ namespace Interfaz
 
                 }
 
-            }
+            }*/
         }
 
     }
