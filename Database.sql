d3c34987c311b917043da6b90b38d2be77585def
aso38-ua <78725192+aso38-ua@users.noreply.github.com>
2023-05-11 02:46:32 +0200

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -8,3 +8,31 @@ CREATE TABLE [dbo].[usuario] (
     [esAdmin]      BIT             DEFAULT ((0)) NOT NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+CREATE TABLE producto (
+    id INT IDENTITY(1, 1) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(500) NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    cantidad INT NOT NULL,
+    imagen VARBINARY(MAX) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE carrito (
+    id INT IDENTITY(1, 1) NOT NULL,
+    usuario_id INT NOT NULL,
+    producto_id INT NOT NULL,
+    cantidad INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+    FOREIGN KEY (producto_id) REFERENCES producto(id)
+);
+
+CREATE TABLE pedido (
+    id INT IDENTITY(1, 1) NOT NULL,
+    usuario_id INT NOT NULL,
+    fecha_pedido DATETIME NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
