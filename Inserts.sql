a89ac7f5cf48df9096c1bc3cf0c4a557638c581d
aso38-ua <78725192+aso38-ua@users.noreply.github.com>
2023-05-26 17:40:18 +0200

    Update Inserts.sql

--- Inserts.sql
+++ Inserts.sql
@@ -68,6 +68,10 @@ INSERT INTO Producto (codigo, nombre, descripcion, stock, precio, codigoCategori
 INSERT INTO Producto (codigo, nombre, descripcion, stock, precio, codigoCategoria, img) VALUES (6, 'Chaqueta', 'Chaqueta de cuero estilo motorista', 1, 45.95, 2, '/IMGS/cuero1.jpg');
 INSERT INTO Producto (codigo, nombre, descripcion, stock, precio, codigoCategoria, img) VALUES (7, 'Tatuaje', 'Tatuajes para tu pez más macarra', 6, 69.99, 2, '/IMGS/koi.jpg');
 INSERT INTO Producto (codigo, nombre, descripcion, stock, precio, codigoCategoria, img) VALUES (8, 'Chaqueta', 'Chaqueta fresquita perfecta para nuestros grandes mamíferos', 6, 125.99, 2, '/IMGS/ballena.jpg');
+INSERT INTO Producto (codigo, nombre, descripcion, stock, precio, codigoCategoria, img) VALUES (9, 'Chaqueta', 'Chaqueta elegante negra', 6, 225.99, 2, '/IMGS/pez_elegante.jpg');
+INSERT INTO Producto (codigo, nombre, descripcion, stock, precio, codigoCategoria, img) VALUES (10, 'Sudadera', 'Sudadera negra con capucha (apertura para aleta)', 6, 29.99, 2, '/IMGS/tiburon_".jpeg');
+INSERT INTO Producto (codigo, nombre, descripcion, stock, precio, codigoCategoria, img) VALUES (11, 'Gafas', 'Monóculo para peces pequeños', 6, 9.99, 2, '/IMGS/payaso_".jpg');
+INSERT INTO Producto (codigo, nombre, descripcion, stock, precio, codigoCategoria, img) VALUES (12, 'Chaqueta', 'Chaqueta de cuero ligera para entretiempo', 4, 29.99, 2, '/IMGS/cuero2_".jpg');
 
 
 INSERT INTO Producto (codigo, nombre, descripcion, precio, stock, codigoCategoria, img) VALUES (21, 'Bañamos a tu pez', 'Servicio de baño relajante y terapéutico para peces', 15.99, 500, 3, '/IMGS/banyo.jfif');
